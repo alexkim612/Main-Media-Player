8d1022065c8e24c4780e91eacae17ba47609fece
Alex Kim <alexkim612@gmail.com>
2020-01-26 18:21:04 -0800

    Added new components for organization purposes, open server 9000

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,25 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 9000
+const path = require('path');
+const morgan = require('morgan');
+
+//See all incoming requests
+app.use(morgan('dev'));
+
+//serve files from dist
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//notification on open server
+app.listen(port, () => console.log(` listening on port ${port}!`))
