d7a8add23112d3f1c2954053075e43089544d1f8
Alex Kim <alexkim612@gmail.com>
2020-01-29 18:36:54 -0800

    Start on aws integration with real db seed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -63,6 +63,8 @@ class App extends React.Component {
           {/* Comments */}
         </WaveFormComments>
 
+        {/* <button>SONG CHANGE</button> */}
+
       </MainPlayerWrapper>
     );
   }
